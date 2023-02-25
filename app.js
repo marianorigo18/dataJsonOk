cdcfd1a9f6be4cabe76881b6b7ff1f6868c50673
marianorigo18 <marianowagnerrigo@gmail.com>
2023-02-24 21:47:21 -0300

    fix vommit

--- app.js
+++ app.js
@@ -1,13 +1,24 @@
 getData()
 async function getData(){
-    const response = await fetch("http://127.0.0.1:5500/data/data.json");
+    const response = await fetch("http://127.0.0.1:5500/dataJsonOk/data/data.json");
     const json = await response.json();
     recorrerData(json);
 }
 
 function recorrerData(data){
-    data.categorias.forEach(element => {
+    data.categorias.forEach((element) => {
+        console.log(element);
         console.log(element.title);
+        console.log(element.link);
+        console.log(element.categorias);
+        element.categorias.forEach(element => {
+            console.log(element.title);
+            element.categorias.forEach(element => {
+                console.log(element.title);
+            })
+        })
     });
+
 }
+
 

a72f80dcdd2e0788efeca74fe457d9a9b733200f
marianorigo18 <marianowagnerrigo@gmail.com>
2023-02-27 11:59:50 -0300

    monday fourth commit

--- scrips/renderProducts.js
+++ scrips/renderProducts.js
@@ -1,1 +1,12 @@
-console.log('Hello');
+obtenerData()
+async function obtenerData(){
+    const response = await fetch("http://127.0.0.1:5500/dataJsonOk/data/data.json");
+    const data = await response.json();
+    console.log(data.categorias[0].title);
+    capturarElements(data);
+}
+
+function capturarElements(data){
+    const containerCards = document.querySelector("#container__cards");
+    containerCards.innerText = data.categorias[0].title
+}

31ea97cc3347bb757c9231313e483160b142526a
marianorigo18 <marianowagnerrigo@gmail.com>
2023-02-28 19:01:48 -0300

    fix commit

--- app.js
+++ app.js
@@ -1,24 +1,24 @@
-getData()
-async function getData(){
-    const response = await fetch("http://127.0.0.1:5500/dataJsonOk/data/data.json");
-    const json = await response.json();
-    // recorrerData(json);
-}
+// getData()
+// async function getData(){
+//     const response = await fetch("http://127.0.0.1:5500/dataJsonOk/data/data.json");
+//     const json = await response.json();
+//     // recorrerData(json);
+// }
 
-function recorrerData(data){
-    data.categorias.forEach(element => {
-        console.log(element.title);
-        element.categorias.forEach(el => {
-            console.log("___"+ el.title);
-            el.categorias.forEach(elet => {
-                console.log("..............."+elet.title);
-                elet.categorias.forEach(elett => {
-                    console.log("................................."+elett.title);
-                })
-            })
-        })
-    });
+// function recorrerData(data){
+//     data.categorias.forEach(element => {
+//         console.log(element.title);
+//         element.categorias.forEach(el => {
+//             console.log("___"+ el.title);
+//             el.categorias.forEach(elet => {
+//                 console.log("..............."+elet.title);
+//                 elet.categorias.forEach(elett => {
+//                     console.log("................................."+elett.title);
+//                 })
+//             })
+//         })
+//     });
 
-}
+// }
 
 
---
+++ scrips/lamp.js
@@ -0,0 +1,23 @@
+obtenerData()
+async function obtenerData(){
+    const response = await fetch("http://127.0.0.1:5500/dataJsonOk/data/data.json");
+    const data = await response.json();
+    capturarElements(data);
+}
+
+function capturarElements(data){
+    const contenedorCards = document.querySelector('#contenedor-cards');
+    let iluminacion = data.categorias[0].categorias[0].categorias
+    iluminacion.forEach(cat => {
+        const box = document.createElement('div');
+        box.classList = 'box';
+        box.innerHTML = `
+        <a href="${cat.link}">
+            <div class="card">
+                <p>${cat.title}</p>
+            </div>
+        </a>
+        `
+        contenedorCards.appendChild(box)
+    });
+}

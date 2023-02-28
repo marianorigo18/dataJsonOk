7618292b9d5c220aff2c769fd89a13217c76e4f8
marianorigo18 <marianowagnerrigo@gmail.com>
2023-02-28 11:58:55 -0300

    we crate cards with dinamics links

--- scrips/renderProducts.js
+++ scrips/renderProducts.js
@@ -2,11 +2,22 @@ obtenerData()
 async function obtenerData(){
     const response = await fetch("http://127.0.0.1:5500/dataJsonOk/data/data.json");
     const data = await response.json();
-    console.log(data.categorias[0].title);
     capturarElements(data);
 }
 
 function capturarElements(data){
-    const containerCards = document.querySelector("#container__cards");
-    containerCards.innerText = data.categorias[0].title
+    const contenedorCards = document.querySelector('#contenedor-cards');
+    let iluminacion = data.categorias[0].categorias
+    iluminacion.forEach(cat => {
+        const box = document.createElement('div');
+        box.classList = 'box';
+        box.innerHTML = `
+        <a href="${cat.link}">
+            <div class="card">
+                <p>${cat.title}</p>
+            </div>
+        </a>
+        `
+        contenedorCards.appendChild(box)
+    });
 }
